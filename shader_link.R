7d0f3033d187cb51834a4386cc0754108fbd4b5d
unknown <arijg21@gmail.com>
2023-10-01 08:29:33 -0700

    icons and export options

--- shader_link.R
+++ shader_link.R
@@ -2,5 +2,5 @@ shader=(\(path){
   path
   return(function(code){
     write(code,'input.txt')
-    system2('open',paste0(path,'/shader_interface.console.exe'))})
+    system2('open',paste0(path,'/program/shader_windows.console.exe'))})
   })(funr::get_script_path())
